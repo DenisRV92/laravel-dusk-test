d2c0df2d563366d44e965bbf33844b7e0f0a0866
6akcbaHu <7den.r7@gmail.com>
2024-11-01 17:53:01 +0300

    test-commit-phpStorm

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -14,8 +14,7 @@ class ExampleTest extends DuskTestCase
     public function testBasicExample(): void
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/w')
-                ->screenshot('tessss')
+            $browser->visit(env('APP_URL'))
                 ->assertSee('Laravel');
         });
     }
